8e4aead76b41ac695af50d6edc73ec6e022ea83f
Student <student@itpark.ru>
2019-09-21 09:35:19 +0300

    Версия для трех человек

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log('worked')
+console.log('worked');
+const firstVote = 10;
+const secondVote = 10;
+const thirdVote = 1;
+
+let promoutersCount = 0;
+let detractorsCount = 0;
+
+if (firstVote >=9) {
+    promoutersCount = promoutersCount + 1;
+}
+
+if (firstVote <=6) {
+    detractorsCount = detractorsCount + 1;
+}
+
+if (secondVote >=9) {
+    promoutersCount = promoutersCount + 1;
+}
+
+if (secondVote <=6) {
+    detractorsCount = detractorsCount + 1;
+}
+
+if (thirdVote >=9) {
+    promoutersCount = promoutersCount + 1;
+}
+
+if (thirdVote <=6) {
+    detractorsCount = detractorsCount + 1;
+}
+
+const nps = (promoutersCount/3 - detractorsCount/3) * 100;
+
+console.log(nps);
+console.log(promoutersCount);
+console.log(detractorsCount);
+
+
