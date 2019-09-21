ff15026df4c6cea72cb889c236f33695eafa6e6f
Student <student@itpark.ru>
2019-09-21 10:13:41 +0300

    убрал дублирование fixes #1

--- app.js
+++ app.js
@@ -9,29 +9,22 @@ let detractorsCount = 0;
 if (firstVote >=9) {
     promoutersCount = promoutersCount + 1;
 }
-
 if (firstVote <=6) {
     detractorsCount = detractorsCount + 1;
 }
-
 if (secondVote >=9) {
     promoutersCount = promoutersCount + 1;
 }
-
 if (secondVote <=6) {
     detractorsCount = detractorsCount + 1;
 }
-
 if (thirdVote >=9) {
     promoutersCount = promoutersCount + 1;
 }
-
 if (thirdVote <=6) {
     detractorsCount = detractorsCount + 1;
 }
-
 const nps = (promoutersCount/3 - detractorsCount/3) * 100;
-
 console.log(nps);
 console.log(promoutersCount);
 console.log(detractorsCount);
